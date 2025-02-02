9220210f49cedde9687fc1c76b05f1dd503bee69
GbelCinema <apocalyyypse3228@gmail.com>
2025-02-02 23:15:20 +0700

    deploy

--- README.md
+++
@@ -1,5 +0,0 @@
-# React + TypeScript + Vite
-
-## npm i, npm run dev:full
-
-### mini-app with ws, ts, rtk, material-ui
--- src/Components/Chat/ConnectionState.tsx
+++
@@ -1,8 +0,0 @@
-interface Props {
-  readonly isConnected: boolean;
-}
-
-export function ConnectionState({isConnected}: Props) {
-  console.log(isConnected);
-  return <p>Подкючение к серверу: {"" + isConnected}</p>;
-}
